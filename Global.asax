37f7f2888ae3c7e13c7f06d44d55335ff2ea4e0e
vcorsiIV <S01160644@acad.tri-c.edu>
2017-05-11 15:44:49 -0400

    Final Upload
    Uploading the final files for the Capstone Project

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ErieHack_TeamEdgwater.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ErieHack_TeamEdgwater.Startup))]
+namespace ErieHack_TeamEdgwater
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
